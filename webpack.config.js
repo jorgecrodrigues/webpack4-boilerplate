ee7b0139a9c5f791831ca3e4f3d68ba2df89f61a
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-06-11 08:39:00 -0400

    Adicionado webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FaviconWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
@@ -24,6 +33,8 @@ module.exports = {
         ]
     },
     plugins: [
+        // Limpa o diretório de saída ...
+        // new CleanWebpackPlugin(),
         // Configuração de template para a página inicial ...
         new HtmlWebpackPlugin({
             template: 'src/index.html'
